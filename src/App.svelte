a378eb07279087f76d822b2d45ed07f3f025964c
Alejandro Di Leo <ale.dileo17@gmail.com>
2019-12-27 20:02:13 -0300

    Got IndxedDB working

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,82 @@
 <script>
+
 	let name = 'Ale'
 	let waterGlassesCount = 0;
+	let qty = 345;
+
+
+	let db;
+	let allEntries = [];
+	let request = indexedDB.open('water');
+
+	request.onerror = function(event) {
+		console.error('Database error: ' + event.target.errorCode);
+	}
+
+	request.onsuccess = function(event) {
+		db = event.target.result;
+		let transaction = db.transaction(['entries'], 'readonly');
+		let objectStore = transaction.objectStore('entries');
+		let cursor = objectStore.openCursor();
+
+		cursor.onsuccess = function(event) {
+			let res = event.target.result;
+			if (res) {
+				allEntries.push(res.value);
+				res.continue();
+			}
+		}
+	}
+
+	request.onupgradeneeded = function(event) {
+		let objectStore;
+		db = event.target.result;
+
+		if(!db.objectStoreNames.contains('entries')) {
+			objectStore = db.createObjectStore('entries', { keyPath: 'timestamp' });
+		}
+		
+		objectStore.createIndex("timestamp", "timestamp", { unique: true });
+		objectStore.createIndex("quantity", "quantity", { unique: false });
+	}
+
+	function addEntry(selectedQty) {
+		const transaction = db.transaction(['entries'], 'readwrite');
+		const store = transaction.objectStore('entries');
+
+		const entry = {
+			timestamp: Date.now(),
+			quantity: selectedQty
+		}
+
+		const request = store.add(entry);
+		
+		request.onerror = function(event) {
+			console.log("Sorry, ", error.target.error.name)
+		}
+
+		request.onsuccess = function(event) {
+			console.log('Success! Saved entry: ', JSON.stringify(entry, null, 2))
+		}
+	}
+
 	function handleClick() {
 		waterGlassesCount += 1
+		addEntry(qty)
 	}
+
+	function handleQty(event) {
+		qty = event.target.value;
+	}
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>You've drinked {waterGlassesCount} glasses of water so far!</p>
 	<button on:click={handleClick}>Add glass of water</button>
+	<!-- <input type="number" on:change={handleQty}> -->
+
 </main>
 
 <style>
